922566ecc7f87ed8e51869a48687833a4d26ca07
Ани Арутюнян <arutiunianan@icloud.com>
2024-12-14 04:29:35 +0300

    add image output and comparison

--- blowfish/blowfish.py
+++ blowfish/blowfish.py
@@ -34,40 +34,38 @@ def decrypt_image(encrypted_data):
     
     return unpadded_data
 
-#def calculate_metrics(original_image_path, decrypted_image_data):
-#    # Загружаем оригинальное изображение для сравнения
-#    original_image = Image.open(original_image_path)
-#    original_data = np.array(original_image)
+def calculate_metrics(original_image_path, decrypted_image_data):
+    original_image = Image.open(original_image_path)
+    original_data = np.array(original_image)
 
-#    # Сохраняем расшифрованные данные в временный файл для дальнейшей обработки
-#    temp_decrypted_image_path = 'temp_decrypted_image.png'
-#    with open(temp_decrypted_image_path, 'wb') as temp_file:
-#        temp_file.write(decrypted_image_data)
+    temp_decrypted_image_path = 'temp_decrypted_image.png'
+    with open(temp_decrypted_image_path, 'wb') as temp_file:
+        temp_file.write(decrypted_image_data)
 
-#    decrypted_image = Image.open(temp_decrypted_image_path)
-#    decrypted_data = np.array(decrypted_image)
+    decrypted_image = Image.open(temp_decrypted_image_path)
+    decrypted_data = np.array(decrypted_image)
 
 #    # 1. PSNR (Peak Signal-to-Noise Ratio)
-#    mse = np.mean((original_data - decrypted_data) ** 2)
-#    if mse == 0:
-#        psnr = float('inf')  # Если MSE равно нулю, PSNR бесконечно
-#    else:
-#        psnr = 20 * np.log10(255.0 / np.sqrt(mse))
+    mse = np.mean((original_data - decrypted_data) ** 2)
+    if mse == 0:
+        psnr = float('inf')  # Если MSE равно нулю, PSNR бесконечно
+    else:
+        psnr = 20 * np.log10(255.0 / np.sqrt(mse))
 
 #    # 2. NPCR (Number of Pixels Change Rate)
-#    original_flat = original_data.flatten()
-#    decrypted_flat = decrypted_data.flatten()
-#    npcr = np.sum(original_flat != decrypted_flat) / original_flat.size
+    original_flat = original_data.flatten()
+    decrypted_flat = decrypted_data.flatten()
+    npcr = np.sum(original_flat != decrypted_flat) / original_flat.size
 
 #    # 3. UACI (Unified Average Changing Intensity)
-#    uaci = np.mean(np.abs(original_flat - decrypted_flat)) / 255.0 * 100
+    uaci = np.mean(np.abs(original_flat - decrypted_flat)) / 255.0 * 100
 
-#    print(f"PSNR: {psnr:.2f} dB")
-#    print(f"NPCR: {npcr:.4f}")
-#    print(f"UACI: {uaci:.2f}%")
+    print(f"PSNR: {psnr:.2f} dB")
+    print(f"NPCR: {npcr:.4f}")
+    print(f"UACI: {uaci:.2f}%")
 
 image_path = 'image.png'
-
+    
 encrypted_image_data = encrypt_image(image_path)
 
 with open('encrypted_image.enc', 'wb') as encrypted_file:
@@ -80,11 +78,18 @@ with open('encrypted_image.enc', 'rb') as encrypted_file:
     encrypted_image_data = encrypted_file.read()
 
 decrypted_image_data = decrypt_image(encrypted_image_data)
+print(len(decrypted_image_data))
 
 with open('decrypted_image.png', 'wb') as decrypted_file:
     decrypted_file.write(decrypted_image_data)
 
+with Image.open('decrypted_image.png') as img:
+    width, height = img.size
+    print(width, height)
+    
 print("Изображение расшифровано и сохранено как decrypted_image.png")
 
-# Рассчитываем метрики
-#calculate_metrics(image_path, decrypted_image_data)
+img_enc = Image.fromarray(np.frombuffer(encrypted_image_data[:277**2], dtype=np.uint8).reshape((277, 277)))
+img_enc.save('encrypted_image.png')
+
+calculate_metrics(image_path, decrypted_image_data)
